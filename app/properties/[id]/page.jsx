2ef4663a99a0a0d90f9b3b1c848f94467ec3b0fe
Param Attri <attri.param@gmail.com>
2024-02-29 20:25:30 +0530

    fetches single property data

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,6 +1,31 @@
-import React from "react";
+"use client";
+
+import { fetchProperty } from "@/utils/requests";
+import { useParams } from "next/navigation";
+import { useState } from "react";
 
 const PropertyPage = () => {
+  const { id } = useParams();
+
+  const [property, setProperty] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPropertyData = async () => {
+      if (!id) return;
+      try {
+        const property = await fetchProperty(id);
+        setProperty(property);
+      } catch (error) {
+        console.error("Error fetching property:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (property === null) fetchPropertyData();
+  }, [id, property]);
+
   return <div>PropertyPage</div>;
 };
 
