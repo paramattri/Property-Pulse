2c2818f40d2362f1298c2bf94e535d2e1fa5b0d0
Param Attri <attri.param@gmail.com>
2024-03-09 21:56:08 +0530

    fetches and display messages

--- app/api/messages/route.js
+++ app/api/messages/route.js
@@ -4,6 +4,33 @@ import { getSessionUser } from "@/utils/getSessionUser";
 
 export const dynamic = "force-dynamic";
 
+export const GET = async (request) => {
+  try {
+    await connectDB();
+
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.user) {
+      return new Response(JSON.stringify("User ID is required"), {
+        status: 401,
+      });
+    }
+
+    const { userId } = sessionUser;
+
+    const messages = await Message.find({ recipient: userId })
+      .populate("sender", "username")
+      .populate("property", "name");
+
+    return new Response(JSON.stringify(messages), { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new Response("Something went wrong", {
+      status: 500,
+    });
+  }
+};
+
 export const POST = async (request) => {
   try {
     await connectDB();
